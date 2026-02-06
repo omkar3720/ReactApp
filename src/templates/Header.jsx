fba44a14d6cdd5862b35a701bf30ad88841e35c2
Omkar Balkrishna Shinde <omkarshinde2728@gmail.com>
2026-02-06 12:47:56 +0530

    Update Header.jsx

--- src/templates/Header.jsx
+++ src/templates/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="container-fluid bg-dark text-white py-3 px-4 d-flex justify-content-between align-items-center">
-      <h1 className="mb-0">StudentManagementApp</h1>
+      <h1 className="mb-0">StudentManagementApp 06/02/2026</h1>
       <div>
         <Link className="btn btn-outline-light me-2" to={"/add"}>
           Add Student
